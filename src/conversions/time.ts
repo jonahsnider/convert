4774f1f9b345ee2bce40b9551eefb4abc992692b
Jonah Snider <jonah@jonah.pw>
2020-05-17 03:11:58 -0700

    feat(time): add more time conversions

--- src/conversions/time.ts
+++ src/conversions/time.ts
@@ -1,10 +1,23 @@
 import {Unit} from '..';
 
 /** Base unit for time. */
-export type TimeBase = 'seconds' | 's';
+export type TimeBase = 'second' | 's';
 
 /** Conversions for time. */
 export const time: Unit<TimeBase> = {
-	base: ['seconds', 's'],
-	conversions: {minutes: 1 / 60, hours: 1 / 60 / 60, days: 1 / 60 / 60 / 24}
+	base: ['second', 's'],
+	conversions: {
+		century: 1 / (3.1556952 * 10e8),
+		day: 1 / (86.4 * 10e2),
+		decade: 1 / (315.56952 * 10e5),
+		fortnight: 1 / (1.2096 * 10e5),
+		hour: 1 / 60 / 60,
+		jiffy: 1 / 60,
+		millenium: 1 / (31.556952 * 10e8),
+		milliday: 1 / 86.4,
+		minute: 1 / 60,
+		month: 1 / (2.592 * 10e6),
+		week: 1 / (604.8 * 10e2),
+		year: 1 / (31.536 * 1e6)
+	}
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -4,33 +4,54 @@ describe('index', () => {
 	it('converts time', () => {
 		expect(
 			convert(1)
-				.from('seconds')
-				.to('seconds')
+				.from('second')
+				.to('second')
 		).toBe(1);
 		expect(
+			convert(1)
+				.from('century')
+				.to('second')
+		).toBe(3.1556952 * 1e9);
+		expect(
 			convert(60)
-				.from('seconds')
-				.to('minutes')
+				.from('second')
+				.to('minute')
 		).toBe(1);
+		expect(
+			convert(1)
+				.from('day')
+				.to('second')
+		).toBe(86400);
+		expect(
+			convert(1)
+				.from('week')
+				.to('second')
+		).toBe(604800);
+		expect(
+			convert(1)
+				.from('year')
+				.to('second')
+		).toBe(31.536 * 1e6);
 	});
+
 	it('supports `BigInt`s', () => {
 		expect(
 			convert(BigInt(2))
-				.from('minutes')
-				.to('seconds')
+				.from('minute')
+				.to('second')
 		).toBe(BigInt(120));
 	});
 
 	it('throws when appropriate', () => {
 		expect(() =>
 			convert(60)
-				.from('seconds')
+				.from('second')
 				.to('not a real value')
 		).toThrow();
 		expect(() =>
 			convert(60)
 				.from('not a real value')
-				.to('minutes')
+				.to('minute')
 		).toThrow();
 	});
 });
