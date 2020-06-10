94a7db3f09d60a198f03016eae85e44d1c9eba79
Jonah Snider <jonah@jonah.pw>
2020-06-10 06:38:41 -0700

    feat(data): add digital data conversion

---
+++ src/conversions/data.ts
@@ -0,0 +1,30 @@
+import {Unit} from '../convert';
+import {binaryPrefixes, prefixer, siPrefixes} from '../util';
+
+/**
+ * Base unit for lengths;
+ * @private
+ */
+export type BytesBase = Readonly<'byte' | 'bytes' | 'B'>;
+type BitsBase = Readonly<'bit' | 'bits' | 'b'>;
+
+/**
+ * Conversions for data.
+ * @private
+ */
+export const data: Unit<BytesBase> = {
+	base: ['byte', 'bytes', 'B'] as const,
+	conversions: [
+		{aliases: ['bit', 'bits', 'b'], ratio: 1 / 8},
+		{aliases: ['crumb', 'crumbs'], ratio: 1 / 4},
+		{aliases: ['nibble', 'nibbles'], ratio: 1 / 2},
+		{aliases: ['word', 'words'], ratio: 2},
+		...prefixer<BytesBase>(
+			{full: 'byte', symbol: 'B'},
+			// None of the tiny prefixes
+			siPrefixes.filter(prefix => prefix.ratio > 1)
+		),
+		...prefixer<BytesBase>({full: 'byte', symbol: 'B'}, binaryPrefixes),
+		...prefixer<BitsBase>({full: 'bit', symbol: 'b'}, binaryPrefixes)
+	]
+};
--- src/unitFamilies.ts
+++ src/unitFamilies.ts
@@ -1,8 +1,9 @@
+import {data} from './conversions/data';
 import {length} from './conversions/length';
 import {time} from './conversions/time';
 import {Unit} from './convert';
 
 /** Families of units, like length or time. */
-export const unitFamilies: Record<string, Unit> = {time, length};
+export const unitFamilies: Record<string, Unit> = {time, length, data};
 
 export const families = Object.values(unitFamilies);
