e462455222bb614945060a878730a0bee1422a20
Tsumari <Flicksie@users.noreply.github.com>
2020-06-11 13:16:43 -0300

    Remove Reanmur

--- src/conversions/temperature.ts
+++ src/conversions/temperature.ts
@@ -1,8 +1,7 @@
 /** Conversions for temperature. */
 export const temperature = [
     {aliases: ['kelvin', 'k'], ratio: 1},
-    {aliases: ['fahrenheit', 'f'], ratio: ???}, // (K * 1.8) - 459.67
-		{aliases: ['celcius', 'centigrades', 'c'], ratio: ???}, // (K + 273.15)
-    {aliases: ['rankine', 'ra'], ratio: 1.8 },
-    {aliases: ['reanmur', 're'], ratio: 0.8}, // (K -273+15) * .8	
+    {aliases: ['fahrenheit', 'f'], ratio: 1.8, diff: -459.67},
+		{aliases: ['celcius', 'centigrades', 'c'], ratio: 1, diff: 273.15},
+    {aliases: ['rankine', 'ra'], ratio: 1.8 }
 ] as const;
