efac110af2d1918d56ef76a86286f2a5d3e1d5f6
Jonah Snider <jonah@jonah.pw>
2020-06-11 12:24:46 -0700

    fix: fix ESM builds not being generated
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,12 +1,14 @@
 module.exports = {
 	rollup(config, options) {
 		// This function will run for each entry/format/env combination
-		if (options.environment === 'development') {
-			// Redirect dev build to nowhere
-			config.output.file = '/dev/null';
-		} else {
-			// Rename production build to index.js
-			config.output.file = './dist/index.js';
+		if (config.output.file.includes('convert.cjs.development.js') || config.output.file.includes('convert.cjs.production.min.js')) {
+			if (options.environment === 'development') {
+				// Redirect dev build to nowhere
+				config.output.file = '/dev/null';
+			} else {
+				// Rename production build to index.js
+				config.output.file = './dist/index.js';
+			}
 		}
 
 		return config;
