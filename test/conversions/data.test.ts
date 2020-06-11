36d86a8ba93ed5d2b81bd6f9565b2f5509c70210
Jonah Snider <jonah@jonah.pw>
2020-06-11 07:28:24 -0700

    test: add basic tests for digital data
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

---
+++ test/conversions/data.test.ts
@@ -0,0 +1,17 @@
+import {convert} from '../../src';
+
+describe('data', () => {
+	it('converts bytes', () =>
+		expect(
+			convert(8)
+				.from('bits')
+				.to('bytes')
+		).toBe(1));
+
+	it('converts millibits', () =>
+		expect(
+			convert(1_000)
+				.from('millibits')
+				.to('bits')
+		).toBe(1));
+});
