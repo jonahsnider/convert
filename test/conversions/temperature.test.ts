98448d5c7e78c74cf77dbdc30fb1916e8a524f8e
Jonah Snider <jonah@jonah.pw>
2020-06-11 13:18:04 -0700

    test(temperature): allow approx value for delisle -> kelvin
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- test/conversions/temperature.test.ts
+++ test/conversions/temperature.test.ts
@@ -5,6 +5,6 @@ describe('temperature', () => {
 	convertAssert(0, 'celsius', 'kelvin', 273.15);
 	convertAssert(0, 'rankine', 'kelvin', 0);
 	convertAssert(5.67, 'rankine', 'kelvin', 3.15);
-	convertAssert(555, 'delisle', 'kelvin', 3.15);
+	convertAssert(555, 'delisle', 'kelvin', 3.15, true);
 	convertAssert(BigInt(0), 'kelvin', 'kelvin', BigInt(0));
 });
