84db691995b47b6beb7174947b1f25bc7094c2bd
Tsumari <Flicksie@users.noreply.github.com>
2020-06-11 13:24:43 -0300

    Update temperature.ts

--- src/conversions/temperature.ts
+++ src/conversions/temperature.ts
@@ -1,7 +1,7 @@
 /** Conversions for temperature. */
 export const temperature = [
     {aliases: ['kelvin', 'k'], ratio: 1},
-    {aliases: ['fahrenheit', 'f'], ratio: 1.8, diff: -459.67},
-		{aliases: ['celcius', 'centigrades', 'c'], ratio: 1, diff: 273.15},
+    {aliases: ['fahrenheit', 'f'], ratio: 1.8, difference: -459.67},
+		{aliases: ['celcius', 'centigrades', 'c'], ratio: 1, difference: 273.15},
     {aliases: ['rankine', 'ra'], ratio: 1.8 }
 ] as const;
