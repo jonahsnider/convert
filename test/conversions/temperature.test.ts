12af11591df717a3bbb7515dfe90321af90f68dd
Jonah Snider <jonah@jonah.pw>
2020-06-11 13:27:26 -0700

    test(temperature): add another Celsius -> kelvin test
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- test/conversions/temperature.test.ts
+++ test/conversions/temperature.test.ts
@@ -3,6 +3,7 @@ import {convertAssert} from '../util';
 describe('temperature', () => {
 	convertAssert(0, 'celsius', 'fahrenheit', 32, true);
 	convertAssert(0, 'celsius', 'kelvin', 273.15);
+	convertAssert(100, 'celsius', 'kelvin', 373.15);
 	convertAssert(0, 'rankine', 'kelvin', 0);
 	convertAssert(5.67, 'rankine', 'kelvin', 3.15);
 	convertAssert(555, 'delisle', 'kelvin', 3.15, true);
