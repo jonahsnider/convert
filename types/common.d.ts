18d20bd92f348873cd50970feecbc5f8c3a19f1d
Jonah Snider <jonah@jonah.pw>
2020-06-11 05:58:43 -0700

    refactor: use much simpler `GetAliases` type
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- types/common.d.ts
+++ types/common.d.ts
@@ -1,6 +1,3 @@
-/** All built-in properties and methods of an `Array`. */
-export type ArrayBuiltIns = keyof Array<unknown>;
-
 export interface ConverterBody<T, Q> {
 	to(to: T): Q;
 }
@@ -10,21 +7,9 @@ export interface Unit {
 	ratio: number;
 }
 
-type _GetAliases<T extends readonly Unit[]> = {
-	0: {
-		[K in Exclude<keyof T, ArrayBuiltIns>]: T[K] extends {
-			aliases: infer A;
-		}
-			? A
-			: never;
-	};
-	1: _GetAliases<T>[0][keyof _GetAliases<T>[0]];
-	2: _GetAliases<T>[1][Exclude<keyof _GetAliases<T>[1], ArrayBuiltIns>];
-};
-
 /**
  * Get a type of every alias belonging to a unit.
  * @example
  * GetAliases<TimeUnits>
  */
-export type GetAliases<T extends readonly Unit[]> = _GetAliases<T>[2];
+export type GetAliases<T extends readonly Unit[]> = T[number]['aliases'][number];
