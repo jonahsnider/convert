b72063ac8fb1b4f2b64806cf6a2daa8d26649759
Jonah Snider <jonah@jonah.pw>
2020-06-12 13:05:10 -0700

    feat(length): add imperial units

--- src/conversions/length.ts
+++ src/conversions/length.ts
@@ -3,7 +3,7 @@
  * @private
  */
 export const length = [
-	{aliases: ['meter', 'metre', 'm'], ratio: 1},
+	{aliases: ['meter', 'meters', 'metre', 'm'], ratio: 1},
 	{aliases: ['petameter', 'petameters', 'Pm'], ratio: 1e15},
 	{aliases: ['terameter', 'terameters', 'Tm'], ratio: 1e12},
 	{aliases: ['gigameter', 'gigameters', 'Gm'], ratio: 1e9},
@@ -17,5 +17,10 @@ export const length = [
 	{aliases: ['micrometer', 'micrometers', 'μm'], ratio: 0.1e-6},
 	{aliases: ['nanometer', 'nanometers', 'nm'], ratio: 1e-9},
 	{aliases: ['picometer', 'picometers', 'pm'], ratio: 1e-12},
-	{aliases: ['femtometer', 'femtometers', 'fm'], ratio: 1e-15}
+	{aliases: ['femtometer', 'femtometers', 'fm'], ratio: 1e-15},
+	{aliases: ['foot', 'feet', 'ft'], ratio: 0.3048},
+	{aliases: ['inch', 'inches', 'in'], ratio: 0.0254},
+	{aliases: ['yard', 'yards', 'yd'], ratio: 0.9144},
+	{aliases: ['mile', 'miles', 'mi'], ratio: 1609.344},
+	{aliases: ['nautical mile', 'nautical miles', 'M', 'NM', 'nmi'], ratio: 1852}
 ] as const;
