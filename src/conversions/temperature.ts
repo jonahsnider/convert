af1e9a35dc66444a3f3e891e09a0390f49dbd7cd
Yousef Sultan <yousef.su.2000@gmail.com>
2020-06-13 08:42:22 +0300

    Fix temperature conversions being wrong

--- src/conversions/temperature.ts
+++ src/conversions/temperature.ts
@@ -7,8 +7,8 @@ export const temperature = {
 	kelvins: {ratio: 1},
 	K: {ratio: 1},
 
-	fahrenheit: {ratio: 5 / 9, difference: 459.67},
-	F: {ratio: 5 / 9, difference: 459.67},
+	fahrenheit: {ratio: 5 / 9, difference: 241.15},
+	F: {ratio: 5 / 9, difference: 241.15},
 
 	celsius: {ratio: 1, difference: 273.15},
 	C: {ratio: 1, difference: 273.15},
@@ -16,6 +16,6 @@ export const temperature = {
 	rankine: {ratio: 5 / 9},
 	R: {ratio: 5 / 9},
 
-	delisle: {ratio: 3 / -2, difference: 373.15},
-	De: {ratio: 3 / -2, difference: 373.15}
+	delisle: {ratio: -2/3, difference: 373.15},
+	De: {ratio: -2/3, difference: 373.15}
 };
--- test/conversions/temperature.test.ts
+++ test/conversions/temperature.test.ts
@@ -10,7 +10,7 @@ describe('temperature', () => {
 	convertAssert(54, 'rankine', 30, 'kelvin');
 	convertAssert(5.67, 'rankine', 3.15, 'kelvin');
 
-	convertAssert(555, 'delisle', 3.15, 'kelvin');
+	convertAssert(555, 'delisle', 3.15, 'kelvin', true);
 
 	convertAssert(BigInt(0), 'kelvin', BigInt(0), 'kelvin');
 });
