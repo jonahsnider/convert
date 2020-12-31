3fb1e45b7d27ce38ceb3dbc06f08ff22368be138
Jonah Snider <jonah@jonah.pw>
2020-12-30 19:51:21 -0800

    test(convert): add test for when both units being converted don't exist

--- test/convert.test.ts
+++ test/convert.test.ts
@@ -14,6 +14,9 @@ describe('convert', () => {
 		// @ts-expect-error
 		expect(() => convert(60).from('kilogram').to('minute')).toThrow();
 
+		// @ts-expect-error
+		expect(() => convert(1).from('not a unit').to('also not a unit')).toThrow();
+
 		expect(() => convert(BigInt(1)).from('grams').to('kilogram')).toThrow("Conversion for grams to kilogram can't be expressed as an integer");
 
 		expect(() => {
