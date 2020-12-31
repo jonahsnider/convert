12182ed56656fe5bb7a5e9395abae7d458563ba3
Jonah Snider <jonah@jonah.pw>
2020-12-30 23:43:15 -0800

    style: remove trailing zeroes

--- test/conversions/force.test.ts
+++ test/conversions/force.test.ts
@@ -4,5 +4,5 @@ describe('force', () => {
 	convertAssert(1, 'newton', 1e5, 'dyn', true);
 	convertAssert(1, 'kp', 9.80665, 'newton');
 	convertAssert(1, 'lbf', 444822.2, 'dyn');
-	convertAssert(1, 'poundal', 0.1382550, 'newton');
+	convertAssert(1, 'poundal', 0.138255, 'newton');
 });
