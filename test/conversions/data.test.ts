4b3ca1bd282dbb57a12f4eb038c252d5a1dc30e1
Jonah Snider <jonah@jonah.pw>
2020-12-30 20:34:06 -0800

    test(data): add tests for development mode

--- test/conversions/data.test.ts
+++ test/conversions/data.test.ts
@@ -4,6 +4,20 @@ import {convertAssert} from '../util';
 describe('data', () => {
 	it('converts bytes', () => expect(convert(8).from('bits').to('bytes')).toBe(1));
 
+	it('converts bytes in production mode', () => {
+		// @ts-expect-error
+		global.__DEV__ = false;
+
+		expect(convert(8).from('bits').to('bytes')).toBe(1);
+	});
+
+	it('converts bytes in dev mode', () => {
+		// @ts-expect-error
+		global.__DEV__ = true;
+
+		expect(convert(8).from('bits').to('bytes')).toBe(1);
+	});
+
 	it('converts millibits', () => expect(convert(1_000).from('millibits').to('bits')).toBe(1));
 
 	convertAssert(1, 'gigabyte', 8e9, 'bits');
