6a4516615b4623d9bcea646c074c9e70aef132df
Jonah Snider <jonah@jonah.pw>
2020-12-30 20:59:54 -0800

    docs: add comment for Unit type

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,4 +4,11 @@ export interface ConverterBody<T, Q> {
 	to(to: T): Q;
 }
 
+/**
+ * A single unit.
+ * @example
+ * ```ts
+ * [UnitFamilies.Data, 1, 0];
+ * ```
+ */
 export type Unit = [family: typeof unitFamilies[keyof typeof unitFamilies], ratio: number, difference: number];
