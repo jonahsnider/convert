011f39bb7b4710da78b880dbf116418deaf7cdc0
Jonah Snider <jonah@jonahsnider.com>
2021-08-28 22:52:47 -0700

    fix(types): export `Area` type

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export {convertMany, ms} from './convert-many';
 export {ConversionFamilyId as UnitFamily} from './dev/types/common';
 export {Conversion} from './dev/types/generated';
 export {conversions as allUnits} from './generated/generated';
-export {Angle, Data, Force, Length, Mass, Pressure, Temperature, Time, Unit, Volume} from './types/units';
+export {Angle, Area, Data, Force, Length, Mass, Pressure, Temperature, Time, Unit, Volume} from './types/units';
