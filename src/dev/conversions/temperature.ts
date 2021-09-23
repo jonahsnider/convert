4bd1e93e2777b4c0ff8fee4fcbb5545727207a1e
Jonah Snider <jonah@jonahsnider.com>
2021-09-23 16:02:52 -0700

    fix(temperature): fix a bug where converting `kelvin` would not work

--- src/dev/conversions/temperature.ts
+++ src/dev/conversions/temperature.ts
@@ -4,9 +4,15 @@ export const id = ConversionFamilyId.Temperature;
 
 export const best: BestConversions = ['C'];
 
+const enum KelvinNames {
+	kelvin = 'kelvin',
+	kelvins = 'kelvins',
+	K = 'K'
+}
+
 /** @see https://en.wikipedia.org/wiki/Conversion_of_scales_of_temperature#Kelvin */
 export const conversions: ConversionGroup = [
-	{names: ['kelvin', 'kelvins'], symbols: ['K'], ratio: 1},
+	{names: [KelvinNames.kelvin, KelvinNames.kelvins], symbols: [KelvinNames.K], ratio: 1},
 
 	{names: ['fahrenheit'], symbols: ['F'], ratio: 5 / 9, difference: 459.67},
 	{names: ['celsius'], symbols: ['C'], ratio: 1, difference: 273.15},
@@ -15,7 +21,7 @@ export const conversions: ConversionGroup = [
 
 /** This is like a `Set` of aliases except it's an object, so we can use the `in` keyword (ES3 compatibility). */
 export const kelvinsAliases: Record<string, unknown> = {
-	K: 0,
-	Kelvin: 0,
-	kelvins: 0
+	[KelvinNames.kelvin]: 0,
+	[KelvinNames.kelvins]: 0,
+	[KelvinNames.K]: 0
 };
