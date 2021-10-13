12e41bfb8e4f8e279ea8047eb9b8d170cfed185d
Jonah Snider <jonah@jonahsnider.com>
2021-10-12 22:50:45 -0700

    refactor: format generated file

--- src/dev/generate/serialize.ts
+++ src/dev/generate/serialize.ts
@@ -1,3 +1,5 @@
+import * as Prettier from 'prettier';
+import * as prettierConfig from '@jonahsnider/prettier-config';
 import type * as Generated from '../types/generated.js';
 
 function stringify(value: unknown): string {
@@ -13,11 +15,15 @@ export function serialize({
 	best: Generated.Best;
 	temperatureDifferences: Generated.TemperatureDifferences;
 }): string {
-	return [
+	const code = [
 		`export const conversions = {__proto__:null, ${stringify(conversions).slice(1)} as const;`,
 		'',
 		`export const bestUnits = ${stringify(best)} as const;`,
 		'',
 		`export const temperatureDifferences = {__proto__:null, ${stringify(temperatureDifferences).slice(1)} as const;`,
 	].join('\n');
+
+	const formatted = Prettier.format(code, {...prettierConfig, filepath: 'generated.ts'});
+
+	return formatted;
 }
---
+++ types/jonahsnider__prettier-config.d.ts
@@ -0,0 +1,3 @@
+/* eslint-disable unicorn/filename-case */
+
+declare module '@jonahsnider/prettier-config';
