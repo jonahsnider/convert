63397a18b1bdd6c736cc3091d9b4259886a15332
Jonah Snider <jonah@jonahsnider.com>
2021-10-13 00:31:49 -0700

    test: run macros in production and development mode

--- src/test/macros.ts
+++ src/test/macros.ts
@@ -13,7 +13,13 @@ export const convert: Macro<[actual: [fromQuantity: number | bigint, from: Unit]
 	const [fromQuantity, fromUnit] = input;
 	const [toQuantity, toUnit] = expected;
 
-	t.is(lib.convert(fromQuantity, fromUnit as any).to(toUnit as any), toQuantity as any);
+	const test = (devFlag: boolean) => {
+		__DEV__ = devFlag;
+		t.is(lib.convert(fromQuantity, fromUnit as any).to(toUnit as any), toQuantity as any);
+	};
+
+	test(false);
+	test(true);
 };
 
 convert.title = (_providedTitle, input, expected) => `${input[0]} ${input[1]} -> ${expected[0]} ${expected[1]}`;
@@ -21,7 +27,13 @@ convert.title = (_providedTitle, input, expected) => `${input[0]} ${input[1]} ->
 export const convertMany: Macro<[actual: string, expected: [quantity: number | bigint, unit: Unit]]> = (t, input, expected) => {
 	const [toQuantity, toUnit] = expected;
 
-	t.is(lib.convertMany(input).to(toUnit as any), toQuantity as any);
+	const test = (devFlag: boolean) => {
+		__DEV__ = devFlag;
+		t.is(lib.convertMany(input).to(toUnit as any), toQuantity as any);
+	};
+
+	test(false);
+	test(true);
 };
 
 convertMany.title = (_providedTitle, input, expected) => `${input} -> ${expected[0]} ${expected[1]}`;
@@ -33,14 +45,26 @@ export const convertManyBest: Macro<[input: [from: string, kind?: BestConversion
 ) => {
 	const {toString, ...best} = lib.convertMany(input[0]).to('best', input[1]);
 
-	t.deepEqual(best, {quantity: expected[0], unit: expected[1]});
-	t.is(toString(), `${expected[0]}${expected[1]}`);
+	const test = (devFlag: boolean) => {
+		__DEV__ = devFlag;
+		t.deepEqual(best, {quantity: expected[0], unit: expected[1]});
+		t.is(toString(), `${expected[0]}${expected[1]}`);
+	};
+
+	test(false);
+	test(true);
 };
 
 convertManyBest.title = (_providedTitle, input, expected) => `${input[0]} -> ${input[1] ?? '(default)'} ${expected[0]} ${expected[1]}`;
 
 export const ms: Macro<[from: string, expected: number | bigint]> = (t, input, expected) => {
-	t.is(lib.ms(input), expected);
+	const test = (devFlag: boolean) => {
+		__DEV__ = devFlag;
+		t.is(lib.ms(input), expected);
+	};
+
+	test(false);
+	test(true);
 };
 
 ms.title = (_providedTitle, input, expected) => `${input} -> ${expected} ms`;
