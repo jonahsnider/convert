8d19a0e394ea92d4587cad91df2baf187759d7bc
Jonah Snider <jonah@jonahsnider.com>
2022-06-10 12:24:40 -0700

    refactor: comply with lint rules

--- packages/convert/src/entry.ts
+++ packages/convert/src/entry.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports, node/prefer-global/process
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports, n/prefer-global/process
 module.exports = require(process.env.NODE_ENV === 'development' ? './convert.dev.js' : './convert.prod.js');
