077abbbe9890db3aa39e1d36a968d7bbb9513ec5
Jonah Snider <jonah@jonahsnider.com>
2022-12-11 17:07:27 -0800

    feat(force): add SI prefixes for newtons

--- packages/conversions/src/conversions/force.ts
+++ packages/conversions/src/conversions/force.ts
@@ -1,4 +1,5 @@
 import type {ReadonlyDeep} from 'type-fest';
+import {expandMacro, Macros} from '../macros/index.js';
 import type {Family} from '../types/index.js';
 import {Id} from '../types/index.js';
 
@@ -7,6 +8,8 @@ export const force: ReadonlyDeep<Family> = {
 	best: {metric: ['N'], imperial: ['lbf']},
 	conversions: [
 		{names: ['newton', 'newtons'], symbols: ['N'], ratio: 1},
+		...expandMacro(Macros.si, {names: ['newton', 'newtons'], symbols: ['N'], ratio: 1}),
+
 		{names: ['dyne', 'dynes'], symbols: ['dyn'], ratio: 1e-5},
 		{names: ['kilogram-force'], symbols: ['kgf', 'kilopond', 'kiloponds', 'kp'], ratio: 9.806_65},
 		{names: ['pound of force', 'pound-force'], symbols: ['lbf'], ratio: 4.448_222},
