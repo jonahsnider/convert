994fca792c1f34af61fbb45d338f2e1196e1d422
Sean Haugh <sean@lfo.team>
2024-09-11 04:48:14 -0500

    feat(energy): update "best" array to use SI prefixed units (#679)

--- src/conversions/measures/energy.ts
+++ src/conversions/measures/energy.ts
@@ -4,13 +4,21 @@ import { type Measure, MeasureKind } from '../types';
 
 export const energy: Measure = {
 	kind: MeasureKind.Energy,
-	best: ['J'],
+	best: ['GWh', 'MWh', 'kWh', 'Wh', 'J'],
 	units: [
 		{ names: ['joule', 'joules'], symbols: ['J'], ratio: 1 },
-		...expandMacro(Macros.si, { names: ['joule', 'joules'], symbols: ['J'], ratio: 1 }),
+		...expandMacro(Macros.si, {
+			names: ['joule', 'joules'],
+			symbols: ['J'],
+			ratio: 1,
+		}),
 
 		// https://en.wikipedia.org/wiki/Kilowatt-hour
 		{ names: ['watt-hour'], symbols: ['W⋅h', 'W h', 'Wh'], ratio: 3600 },
-		...expandMacro(Macros.si, { names: ['watt-hour', 'watt-hours'], symbols: ['W⋅h', 'W h', 'Wh'], ratio: 3600 }),
+		...expandMacro(Macros.si, {
+			names: ['watt-hour', 'watt-hours'],
+			symbols: ['W⋅h', 'W h', 'Wh'],
+			ratio: 3600,
+		}),
 	],
 };
