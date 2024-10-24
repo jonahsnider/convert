d06faffda6cbb6acdb56b8d6d420141ccbfa121d
Jonah Snider <jonah@jonahsnider.com>
2024-10-24 01:16:06 -0700

    refactor: fix lint warnings

--- src/converters/conversions/energy.test.ts
+++ src/converters/conversions/energy.test.ts
@@ -7,6 +7,7 @@ describe('conversions', () => {
 	assertConversions([{ from: [1000, 'J'], to: [1, 'kilojoule'] }]);
 	assertConversions([{ from: [1000000, 'kWh'], to: [1, 'GWh'] }]);
 
+	// biome-ignore lint/style/useNamingConvention: This is in camelcase
 	const best1GWh = convert(1000000, 'kWh').to('best').toString();
 	const best1Wh = convert(3600, 'J').to('best').toString();
 
