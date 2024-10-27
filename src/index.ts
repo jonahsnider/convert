dd535ccec9397dbe6218d28a6a02d3d9bdbaea98
Matt Hink <mhink1103@gmail.com>
2024-10-26 18:33:26 -0700

    feat: add getMeasureKind() function for validating inputs
    Co-authored-by: Jonah Snider <jonah@jonahsnider.com>

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export type { BestKind, MeasureKind } from './conversions/types';
 export { convertMany } from './converters/convert-many';
 // biome-ignore lint/style/noDefaultExport: This is a default export we want
 export { convert, convert as default } from './converters/convert';
+export { getMeasureKind } from './converters/get-measure-kind';
 export { ms } from './converters/ms';
 export type { UnitsByMeasure as _UnitsByMeasureRaw } from './generated/types';
 export type { BestConversion, Converter } from './types/converter';
