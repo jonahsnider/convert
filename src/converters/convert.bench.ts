da19d1eaa0d8541c138b9ec2b750397ef9798984
Jonah Snider <jonah@jonahsnider.com>
2024-10-31 23:45:19 -0700

    test: improve benchmark titles

--- src/converters/convert.bench.ts
+++ src/converters/convert.bench.ts
@@ -2,19 +2,19 @@ import { bench, describe } from 'vitest';
 import { convert } from './convert';
 
 describe('convert to best', () => {
-	bench('8192 bytes', () => {
+	bench('convert 8192 bytes to best', () => {
 		convert(8192, 'bytes').to('best');
 	});
 });
 
 describe('convert units', () => {
-	bench('24 hours to minutes', () => {
+	bench('convert 24 hours to minutes', () => {
 		convert(24, 'hours').to('minutes');
 	});
-	bench('4 inches to millimeters', () => {
+	bench('convert 4 inches to millimeters', () => {
 		convert(4, 'inches').to('millimeters');
 	});
-	bench('2.5 liters to in3', () => {
+	bench('convert 2.5 liters to in3', () => {
 		convert(2.5, 'liters').to('in3');
 	});
 });
--- src/converters/ms.bench.ts
+++ src/converters/ms.bench.ts
@@ -2,7 +2,11 @@ import { bench, describe } from 'vitest';
 import { ms } from './ms';
 
 describe('ms', () => {
-	bench('10h', () => {
+	bench('parse 10h to ms', () => {
 		ms('10h');
+	});
+
+	bench('parse 10h in ms to string', () => {
+		ms(36_000_000);
 	});
 });
