469b84df786d6cd47dfe37ac745761c24cea0117
Jonah Snider <jonah@jonahsnider.com>
2026-02-20 23:29:07 -0800

    fix(luminance): update candela unit names and symbols for consistency
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/conversions/measures/luminance.ts
+++ src/conversions/measures/luminance.ts
@@ -6,32 +6,49 @@ export const luminance: Measure = {
 	best: ['cd/m²'],
 	units: [
 		// https://en.wikipedia.org/wiki/Candela_per_square_metre
-		{ names: ['candela per square metre'], symbols: ['cd/m²'], ratio: 1 },
-		...expandMacro(Macros.si, { names: ['candela per square metre'], symbols: ['cd/m²'], ratio: 1 }),
+		{
+			names: [
+				'candela per square meter',
+				'candelas per square meter',
+				'candela per square metre',
+				'candelas per square metre',
+			],
+			symbols: ['cd/m2', 'cd/m²'],
+			ratio: 1,
+		},
+		...expandMacro(Macros.si, {
+			names: [
+				'candela per square meter',
+				'candelas per square meter',
+				'candela per square metre',
+				'candelas per square metre',
+			],
+			symbols: ['cd/m2', 'cd/m²'],
+			ratio: 1,
+		}),
 
-		// Wikipedia says that "nit" is deprecated, but it's still widely used,
-		// for things like display brightness for example
+		// Wikipedia says that "nit" is deprecated, but it's still widely used, for things like display brightness for example
 		// (see e.g. https://www.apple.com/iphone-16/specs/#:~:text=1000%20nits%20max%20brightness).
-		{ names: ['nit'], symbols: ['nt'], ratio: 1 },
-		...expandMacro(Macros.si, { names: ['nit'], symbols: ['nt'], ratio: 1 }),
+		{ names: ['nit', 'nits'], symbols: ['nt'], ratio: 1 },
+		...expandMacro(Macros.si, { names: ['nit', 'nits'], symbols: ['nt'], ratio: 1 }),
 
-		{ names: ['lambert'], symbols: [] /* 'L' conflicts with litre */, ratio: 1e4 / Math.PI },
+		{
+			names: ['lambert'],
+			// L conflicts with liter
+			symbols: [],
+			ratio: 1e4 / Math.PI,
+		},
 
 		// https://en.wikipedia.org/wiki/Foot-lambert
 		{
-			names: ['foot-lambert', 'foot lambert'],
-			symbols: [/* 'fL' conflicts with femtolitre, */ 'ft-L'],
+			names: ['foot-lambert', 'footlambert'],
+			// fl and fL conflict with femtoliter
+			symbols: ['ft-L'],
 			ratio: 1 / 0.3048 ** 2 / Math.PI,
 		},
 
-		// https://en.wikipedia.org/wiki/Skot_(unit)
-		{ names: ['skot'], symbols: ['sk'], ratio: 1e-3 / Math.PI },
-
-		// https://en.wikipedia.org/wiki/Bril_(unit)
-		{ names: ['bril'], symbols: ['bl'], ratio: 1e-7 / Math.PI },
-
 		// https://en.wikipedia.org/wiki/Apostilb
-		{ names: ['apostilb', 'blondel'], symbols: ['asb'], ratio: 1 / Math.PI },
+		{ names: ['apostilb'], symbols: ['asb'], ratio: 1 / Math.PI },
 
 		// https://en.wikipedia.org/wiki/Stilb
 		{ names: ['stilb'], symbols: ['sb'], ratio: 1e4 },
