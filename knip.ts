e4b41142aac8f370b892eebca0a0a607ce646860
Jonah Snider <jonah@jonahsnider.com>
2026-02-20 22:35:06 -0800

    build: replace Rollup with zshy
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- knip.ts
+++ knip.ts
@@ -4,9 +4,8 @@ const config: KnipConfig = {
 	ignore: ['docs/**'],
 	entry: ['src/index.ts', 'src/**/*.bench.ts'],
 
-	// Referenced as a string in rollup.config.ts
 	// @vitest/coverage-v8 is dynamically loaded by vitest when coverage.provider is 'v8'
-	ignoreDependencies: ['@babel/preset-env', '@vitest/coverage-v8'],
+	ignoreDependencies: ['@vitest/coverage-v8'],
 };
 
 export default config;
