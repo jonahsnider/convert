55a19eb480d5dc0b1114dc3927bc0763dfcc67d8
Jonah Snider <jonah@jonahsnider.com>
2026-02-23 22:58:18 -0800

    build: remove unnecessary Knip ignore
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- knip.ts
+++ knip.ts
@@ -3,9 +3,6 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
 	ignore: ['docs/**'],
 	entry: ['src/index.ts', 'src/**/*.bench.ts'],
-
-	// @vitest/coverage-v8 is dynamically loaded by vitest when coverage.provider is 'v8'
-	ignoreDependencies: ['@vitest/coverage-v8'],
 };
 
 export default config;
