f572e13cf76f359172e5c5007c1b771bd615b7e4
Jonah Snider <jonah@jonahsnider.com>
2026-08-17 17:22:25 -0700

    docs: remove RunKit example filename
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- docs/runkit.js
+++
@@ -1,8 +0,0 @@
-const { convert, convertMany, ms } = require('convert');
-
-// Convert time, length, data, volume, mass, and more
-convert(1024, 'bytes').to('kB');
-
-// Combine several units into one, with a shorthand for ms
-convertMany('1mo 13d').to('year');
-ms('1mo 13d');
